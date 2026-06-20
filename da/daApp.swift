f006b8d821830ad9524888f6b10a1b07d2de4c42
5526 <5526>
2026-06-20 17:25:15 +0300

    Initial Commit

---
+++ da/daApp.swift
@@ -0,0 +1,17 @@
+//
+//  daApp.swift
+//  da
+//
+//  Created by Динара Ибрагимова on 20.06.2026.
+//
+
+import SwiftUI
+
+@main
+struct daApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ daTests/daTests.swift
@@ -0,0 +1,19 @@
+//
+//  daTests.swift
+//  daTests
+//
+//  Created by Динара Ибрагимова on 20.06.2026.
+//
+
+import Testing
+@testable import da
+
+struct daTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
